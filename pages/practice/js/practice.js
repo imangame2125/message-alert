891a55a743a6af28b6c1cb72414babeb123ec718
Iman Majdabadi <imanclever51@gmail.com>
2021-11-16 12:09:18 +0330

    pages

--- pages/practice/js/practice.js
+++ pages/practice/js/practice.js
@@ -1,15 +1,17 @@
-let selected = document.querySelector('#selected')
-selected.style.backgroundColor = 'white'
-selected.style.color = 'black'
-selected.addEventListener('click', () => {
-   selected.style.backgroundColor = 'white'
-    selected.style.color = 'black'
-  document.body.style.backgroundColor = selected.value
-  if (selected.value === '') {
-    selected.style.backgroundColor = 'white'
-    selected.style.color = 'black'
-  } else {
-    selected.style.backgroundColor = selected.value
-    selected.style.color = 'black'
-  }
-})
+// let selected = document.querySelector('#selected')
+// selected.style.backgroundColor = 'white'
+// selected.style.color = 'black'
+// selected.addEventListener('click', () => {
+//    selected.style.backgroundColor = 'white'
+//     selected.style.color = 'black'
+//   document.body.style.backgroundColor = selected.value
+//   if (selected.value === '') {
+//     selected.style.backgroundColor = 'white'
+//     selected.style.color = 'black'
+//   } else {
+//     selected.style.backgroundColor = selected.value
+//     selected.style.color = 'black'
+//   }
+// })
+
+
