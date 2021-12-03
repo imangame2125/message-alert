84cd26f08003d446a8f1d631edae49d39348f818
Iman Majdabadi <imanclever51@gmail.com>
2021-12-03 15:07:07 +0330

    more

--- pages/average/js/average.js
+++ pages/average/js/average.js
@@ -9,8 +9,7 @@ function average(a, b) {
   return numbers
 }
 
-
-button.addEventListener('click', () => {
+function myWork() {
   let math = mathInput.value
   let law = lawInput.value
 
@@ -20,10 +19,38 @@ button.addEventListener('click', () => {
     resultAverage.textContent = average(math, law)
   }
 
-  if (resultAverage.textContent > 10) {
+  if (resultAverage.textContent >= 10) {
     result.textContent = 'ghabool'
   } else {
     result.textContent = 'mardood'
   }
+}
+
+button.addEventListener('click', () => {
+  myWork()
 })
 
+const names = ['ali', 'naser', 'sara', 'fatemeh', 'sahar'];
+let input = document.createElement('input');
+const btn = document.createElement('button');
+const javab = document.createElement('p')
+
+document.body.appendChild(input);
+document.body.appendChild(javab);
+document.body.appendChild(btn)
+
+input.type = 'text';
+btn.textContent = 'click'
+btn.addEventListener('click', () => {
+  let inputValue = input.value
+  for (let i = 0; i < names.length; i++) {
+    let element = names[i]
+    if (element === inputValue) {
+      javab.textContent = 'yes'
+      return
+    } else if (i === names.length - 1) {
+      console.log('rrr')
+      javab.textContent = 'no'
+    }
+  }
+})
